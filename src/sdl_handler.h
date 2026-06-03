24ab7ed746199a741c8697854a19cfa2ba3862f6
Void <112571254+TheVoidMonarch@users.noreply.github.com>
2026-06-03 19:44:48 +0800

    Add stop_azan function and modify render_text
    Added stop_azan function and updated render_text signature.

--- src/sdl_handler.h
+++ src/sdl_handler.h
@@ -4,30 +4,25 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
 #include "api_handler.h"
 
-// Function to initialize SDL video and create a window
+// Video functions
 SDL_Window* init_sdl_video(int width, int height);
-
-// Function to initialize SDL audio
-int init_sdl_audio();
-
-// Function to quit SDL audio
-void quit_sdl_audio();
-
-// Function to render text on the SDL window
-void render_text(SDL_Renderer* renderer, TTF_Font* font, const char* text, int x, int y, SDL_Color color);
-
-// Function to play Azan audio
-void play_azan(const char* audio_path);
-
-// Function to check if azan is currently playing
-int is_azan_playing();
-
-// Function to display prayer times graphically (for local execution)
-void displayGraphicalPrayerTimesLocal(PrayerTimes pt);
-
-// Function to close SDL video
 void close_sdl_video(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font);
+
+// Audio functions
+int init_sdl_audio();
+void quit_sdl_audio();
+void play_azan(const char* audio_path);
+int is_azan_playing();
+void stop_azan();   // NEW
+
+// Text rendering
+void render_text(SDL_Renderer* renderer, TTF_Font* font, const char* text,
+                 int x, int y, SDL_Color color);
+
+// Graphical prayer times
+void displayGraphicalPrayerTimesLocal(PrayerTimes pt);
 
 #endif // SDL_HANDLER_H
