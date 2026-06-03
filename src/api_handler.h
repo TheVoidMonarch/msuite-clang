6e6f9d4af14d6594260dd7203d60c04397647c07
Void <112571254+TheVoidMonarch@users.noreply.github.com>
2026-06-03 16:07:09 +0800

    Add files via upload

---
+++ src/api_handler.h
@@ -0,0 +1,16 @@
+#ifndef API_HANDLER_H
+#define API_HANDLER_H
+
+typedef struct {
+    char fajr[6];
+    char sunrise[6];
+    char dhuhr[6];
+    char asr[6];
+    char maghrib[6];
+    char isha[6];
+} PrayerTimes;
+
+// Function to fetch and parse prayer times from the API
+PrayerTimes fetch_prayer_times(const char *city, const char *country, int method);
+
+#endif // API_HANDLER_H
